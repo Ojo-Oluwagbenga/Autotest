85329dd841c8f8293dbc09215c0e2b23b1c3fb63
Ojo-Oluwagbenga <57333233+Ojo-Oluwagbenga@users.noreply.github.com>
2023-01-28 19:52:28 +0100

    Initial commit on project metamorph

--- main/models.py
+++ main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
+class User(models.Model):
+    itemcode = models.CharField(max_length=20)
+    name = models.CharField(max_length=20)
+    email = models.EmailField()
+    password = models.CharField(max_length=255)
+    user_type = models.CharField(max_length=20)
+    matric = models.CharField(default="non_student", max_length=20)
+
